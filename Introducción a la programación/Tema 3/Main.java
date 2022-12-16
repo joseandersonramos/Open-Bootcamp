36b9cb18d957a04e80e0f0e412c703dd750508ab
José Anderson Ramos Aquino <34531721+joseandersonramos@users.noreply.github.com>
2022-12-15 23:59:21 -0400

    Create Main.java

---
+++ Introducción a la programación/Tema 3/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/*
+Primera parte:
+
+    1-Crear una función con tres parámetros que sean números que se suman entre sí.
+    2-Llamar a la función en el main y darle valores.
+
+Segunda parte:
+
+    1-Crear una clase coche.
+    2-Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+    3-Una función que incremente el número de puertas que tiene el coche.
+    4-Crear un objeto miCoche en el main y añadirle una puerta.
+    5-Mostrar el número de puertas que tiene el objeto.
+*/
+public class Main {
+    public static void main(String[] args){
+
+        suma(1 ,5, 7);
+
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuertas();
+        System.out.println("El numero de puertas es: " + miCoche.puertas);
+    }
+
+    public static void suma(int a, int b, int c){
+        System.out.println(a + b + c);
+    }
+}
+
+class Coche {
+    public int puertas = 4;
+    public void AgregarPuertas() {
+        this.puertas++;
+    }
+}
