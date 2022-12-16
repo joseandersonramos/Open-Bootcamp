68cd7dd4166ac2bb9c2f7dbe0c30d3d1afbddc9e
José Anderson Ramos Aquino <34531721+joseandersonramos@users.noreply.github.com>
2022-12-16 17:10:08 -0400

    Tarea 4

---
+++ Introducción a la programación/Tarea 4/Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+//Ejercicios Tema 4 - Estructuras de control
+
+public class Main {
+    public static void main(String[] args){
+
+        /*
+        1-Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+        */
+
+        int numeroIf = 1;
+
+        if(numeroIf > 0){
+            System.out.println("El número es positivo.");
+        }
+
+        else if(numeroIf < 0){
+            System.out.println("El número es negativo.");
+        }
+
+        else {
+            System.out.println("El número es igual a 0.");
+        }
+
+        /*
+        2-Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile
+         sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+            -Incrementar el valor de la variable en uno cada vez que se ejecute.
+            -Mostrarlo por pantalla cada vez que se ejecute.
+         */
+
+        int numeroWhile = 0;
+
+        while (numeroWhile <3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+
+        /*
+        3-Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se
+         debe ejecutar una vez.
+        */
+
+        numeroWhile = 1;
+
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        } while(numeroWhile < 0);
+
+        /*
+        4-Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición
+        será que la variable sea igual o menor que 3, se irá incrementando en 1 su valor cada vez que se ejecute
+        y deberá mostrarse por pantalla.
+        */
+
+        for (int numeroFor = 0; numeroFor <=3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+        /*
+        5-Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro
+        estaciones del año. Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola
+        informando de la estación en la que está. También habrá que poner un default para cuando el valor de la
+        variable no sea una estación.
+        */
+
+        //primavera, verano, otoño e invierno.
+        String estacion = "primavera";
+
+        switch (estacion){
+            case "primavera":
+                System.out.println("Estamos en primavera!");
+                break;
+            case "verano":
+                System.out.println("Estamos en verano!");
+                break;
+            case "otoño":
+                System.out.println("Estamos en otoño!");
+                break;
+            case "invierno":
+                System.out.println("Estamos en invierno!");
+                break;
+            default:
+                System.out.println("Esta no es una estación!");
+        }
+    }
+}
